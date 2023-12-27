722a962f795b9efd51c2157a12fbdb5141ea399e
Lucas Furlan <furlan@hotmail.com.br>
2023-12-26 23:32:26 -0300

    chore: Production config

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
 
   app.useGlobalFilters(new PrismaNotFoundExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
 bootstrap();
