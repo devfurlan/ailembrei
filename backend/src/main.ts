a569782a6b0660b0704cc53a0d6ec6de34940eaa
Lucas Furlan <furlan@hotmail.com.br>
2023-12-31 09:26:52 -0300

    build: update origin cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://todo.devfurlan.com/'],
+    origin: ['https://ailembrei.com/'],
   });
 
   app.useGlobalPipes(
